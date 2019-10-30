2ae648070ae9d1f521cb7450e9a541804fbd84f7
Matthew Gerstman <mattgerstman@gmail.com>
2019-10-30 12:28:29 +0100

    Personal info

--- snippets/incremental.tsx
+++ snippets/incremental.tsx
@@ -1,25 +1,11 @@
-const muggle = "Vernon Dursley";
+import * as lodash from "lodash";
+
+const muggle = "Dursley Vernon";
 const yellingMuggle = muggle.toUpperCase();
 
 yellingMuggle.slice(0, 10);
 
 type Wizard = {name: string};
-const wizard: Wizard = {};
-wizard.name = "Harry Potter";
+const wizard: Wizard = {name: "Harry Potter"};
 
-// import * as lodash from "lodash";
-
-// const muggle = "Vernon Dursley";
-// const yellingMuggle = muggle.toLocaleUpperCase();
-// yellingMuggle.slice(0, 10);
-
-// type Wizard = {
-//   name: string;
-// };
-
-// const wizard: Wizard = {name: "harry potter"};
-
-// lodash.debounce(() => "foo", 1000);
-
-// lodash.filter([1, 3], val => val > 1);
-// //
+lodash.filter([1, 5, 7, 42], val => val > 10);
